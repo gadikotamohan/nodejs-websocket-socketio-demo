0e6abfa76aef18c08d34da1bc437b7b496c91aef
Gadikota Mohan Raja <mohan@devathon.com>
2016-06-24 23:11:02 +0530

    Added node client for websocket

--- app.js
+++ app.js
@@ -119,4 +119,17 @@ socketio.listen(server1).on('connection', function (socket) {
 
 server1.listen(8081, function () { console.log('SocketIO listening on ' + server1.address().port) });
 
+var WebSocket = require('ws');
+
+var ws = new WebSocket('ws://localhost:8080/hello', 'borf');
+ var util = require('util')
+ws.addListener('data', function(buf) {
+  console.log('Client Got data: ' + util.inspect(buf.data));
+});
+ws.onmessage = function(m) {
+  console.log('Client Got message: ' + util.inspect(m.data));
+}
+ws.addEventListener('open', function(event) {
+  ws.send("ejiejie");
+});
 module.exports = app;
